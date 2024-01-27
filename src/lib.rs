54d6f50c786510f543c5aa415f3c6a43283c0bf9
Philipp Reiter <p.reiter93@gmail.com>
2024-01-27 20:57:33 +0100

    Update demo gif

--- src/lib.rs
+++ src/lib.rs
@@ -73,7 +73,7 @@
 //! The behaviour of [`ListState`] can be modified
 //! - **circular**: Whether the selection is circular, i.e. if true, the first item is selected after the last. True by default.
 //!
-//!![](img/demo.gif)
+//!![](resources/demo.gif)
 pub mod state;
 pub mod traits;
 pub mod widget;
