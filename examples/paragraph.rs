40808b88a881d4f23a566d07f71b6989393c1297
Philipp Reiter <philipp.reiter@freiheit.com>
2024-02-24 16:59:09 +0100

    Remove truncate parameter

--- examples/paragraph.rs
+++ examples/paragraph.rs
@@ -122,8 +122,7 @@ impl<'a> App<'a> {
         ];
         let list = List::new(items)
             .style(Style::default().bg(Color::Black))
-            .block(Block::default().borders(Borders::ALL).title("Outer block"))
-            .truncate(true);
+            .block(Block::default().borders(Borders::ALL).title("Outer block"));
         let state = ListState::default();
         App { list, state }
     }
