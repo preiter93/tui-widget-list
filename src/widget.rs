f6a503b889b6c97cadc291dfb704e0db5b29a561
Philipp Reiter <philipp.reiter@freiheit.com>
2024-05-04 15:39:11 +0200

    Simplify Lists render

--- src/widget.rs
+++ src/widget.rs
@@ -85,15 +85,14 @@ impl<'a, T: PreRender> From<Vec<T>> for List<'a, T> {
 
 impl<'a, T: PreRender> StatefulWidget for List<'a, T> {
     type State = ListState;
-    /// Renders a mutable reference to a widget list
-    #[allow(deprecated)]
+
     fn render(self, area: Rect, buf: &mut Buffer, state: &mut Self::State) {
         let style = self.style;
         let scroll_axis = self.scroll_axis;
 
-        let raw_items = self.items;
+        let mut items = self.items;
         let mut block = self.block;
-        state.set_num_elements(raw_items.len());
+        state.set_num_elements(items.len());
 
         // Set the base style
         buf.set_style(area, style);
@@ -107,7 +106,7 @@ impl<'a, T: PreRender> StatefulWidget for List<'a, T> {
         };
 
         // List is empty
-        if raw_items.is_empty() {
+        if items.is_empty() {
             return;
         }
 
@@ -117,16 +116,10 @@ impl<'a, T: PreRender> StatefulWidget for List<'a, T> {
             ScrollAxis::Horizontal => (area.width, area.height),
         };
 
-        // The starting coordinates of the current item
-        let (mut scroll_axis_pos, cross_axis_pos) = match scroll_axis {
-            ScrollAxis::Vertical => (area.top(), area.left()),
-            ScrollAxis::Horizontal => (area.left(), area.top()),
-        };
-
         // Call the user provided callback to modify the items based on render info
-        let mut items = Vec::new();
-        for (index, mut item) in raw_items.into_iter().enumerate() {
-            let highlighted = state.selected().map_or(false, |j| index == j);
+        let mut main_axis_sizes = Vec::new();
+        for (index, item) in items.iter_mut().enumerate() {
+            let highlighted = state.selected.map_or(false, |j| index == j);
 
             let context = PreRenderContext {
                 cross_axis_size,
@@ -136,59 +129,81 @@ impl<'a, T: PreRender> StatefulWidget for List<'a, T> {
             };
 
             let main_axis_size = item.pre_render(&context);
-            items.push((item, main_axis_size));
+            main_axis_sizes.push(main_axis_size);
         }
 
         // Determine which widgets to show on the viewport and how much space they
-        // get assigned to. The number of elements in `view_heights` is less than
-        // the number of elements in `raw_heights` if not all widgets are shown
-        // on the viewport.
-        let heights: Vec<_> = items.iter().map(|x| x.1).collect();
-        let sizes_scroll_direction = state.update_view_port(&heights, scroll_axis_size);
+        // get assigned to.
+        let available_main_axis_sizes_viewport =
+            state.update_view_port(&main_axis_sizes, scroll_axis_size);
 
         // Drain out elements that are shown on the view port from the vector of
         // all elements.
-        let first = state.offset;
-        let last = sizes_scroll_direction.len() + first;
-        let mut items_in_view = items.drain(first..last);
+        let num_items_viewport = available_main_axis_sizes_viewport.len();
+        let (start, end) = (state.offset, num_items_viewport + state.offset);
+        let items_viewport = items.drain(start..end);
+        let main_axis_sizes_viewport = main_axis_sizes.drain(start..end);
 
-        // Iterate over the modified items
-        let num_items = sizes_scroll_direction.len();
-        for (i, size) in sizes_scroll_direction.into_iter().enumerate() {
+        // The starting coordinates of the current item
+        let (mut scroll_axis_pos, cross_axis_pos) = match scroll_axis {
+            ScrollAxis::Vertical => (area.top(), area.left()),
+            ScrollAxis::Horizontal => (area.left(), area.top()),
+        };
+
+        // Render the widgets on the viewport
+        for (i, (available_main_axis_size, (main_axis_size, item))) in
+            available_main_axis_sizes_viewport
+                .into_iter()
+                .zip(main_axis_sizes_viewport.zip(items_viewport))
+                .enumerate()
+        {
             let area = match scroll_axis {
-                ScrollAxis::Vertical => {
-                    Rect::new(cross_axis_pos, scroll_axis_pos, cross_axis_size, size)
-                }
-                ScrollAxis::Horizontal => {
-                    Rect::new(scroll_axis_pos, cross_axis_pos, size, cross_axis_size)
-                }
+                ScrollAxis::Vertical => Rect::new(
+                    cross_axis_pos,
+                    scroll_axis_pos,
+                    cross_axis_size,
+                    available_main_axis_size,
+                ),
+                ScrollAxis::Horizontal => Rect::new(
+                    scroll_axis_pos,
+                    cross_axis_pos,
+                    available_main_axis_size,
+                    cross_axis_size,
+                ),
             };
-            if let Some(item) = items_in_view.next() {
-                // Check if the item needs to be truncated
-                if area.height < item.1 {
-                    // Determine if truncation should happen at the top or the bottom
-                    let truncate_top = i == 0 && num_items > 1;
-                    render_truncated(item.0, item.1, area, buf, scroll_axis, truncate_top, style);
-                } else {
-                    item.0.render(area, buf);
-                }
+
+            // Check if the item needs to be truncated
+            if available_main_axis_size < main_axis_size {
+                // Determine whether to truncate from the top or the bottom
+                let truncate_top = i == 0 && num_items_viewport > 1;
+                render_truncated(
+                    item,
+                    area,
+                    buf,
+                    main_axis_size,
+                    scroll_axis,
+                    truncate_top,
+                    style,
+                );
+            } else {
+                item.render(area, buf);
             }
 
-            scroll_axis_pos += size;
+            scroll_axis_pos += available_main_axis_size;
         }
     }
 }
 
 /// Renders a listable widget within a specified area of a buffer, potentially truncating the widget content based on scrolling direction.
 /// `truncate_top` indicates whether to truncate the content from the top or bottom.
-fn render_truncated<T: PreRender>(
+fn render_truncated<T: Widget>(
     item: T,
-    main_axis_size: u16,
     area: Rect,
     buf: &mut Buffer,
+    main_axis_size: u16,
     scroll_axis: ScrollAxis,
     truncate_top: bool,
-    base_style: Style,
+    style: Style,
 ) {
     let item_size = main_axis_size;
     // Create an intermediate buffer for rendering the truncated element
@@ -202,7 +217,7 @@ fn render_truncated<T: PreRender>(
         width,
         height,
     });
-    hidden_buffer.set_style(hidden_buffer.area, base_style);
+    hidden_buffer.set_style(hidden_buffer.area, style);
     item.render(hidden_buffer.area, &mut hidden_buffer);
 
     // Copy the visible part from the intermediate buffer to the main buffer
@@ -213,9 +228,10 @@ fn render_truncated<T: PreRender>(
             } else {
                 0
             };
-            for x in area.left()..area.right() {
-                for y in area.top()..area.bottom() {
-                    *buf.get_mut(x, y) = hidden_buffer.get(x, y + offset).clone();
+            for y in area.top()..area.bottom() {
+                let y_off = y + offset;
+                for x in area.left()..area.right() {
+                    *buf.get_mut(x, y) = hidden_buffer.get(x, y_off).clone();
                 }
             }
         }
@@ -226,8 +242,9 @@ fn render_truncated<T: PreRender>(
                 0
             };
             for x in area.left()..area.right() {
+                let x_off = x + offset;
                 for y in area.top()..area.bottom() {
-                    *buf.get_mut(x, y) = hidden_buffer.get(x + offset, y).clone();
+                    *buf.get_mut(x, y) = hidden_buffer.get(x_off, y).clone();
                 }
             }
         }
@@ -303,6 +320,19 @@ mod test {
                 "└───┘",
             ]),
         )
+    }
+
+    #[test]
+    fn empty() {
+        // given
+        let (area, mut buf, _, mut state) = init(3);
+        let list = List::new(Vec::<TestItem>::new());
+
+        // when
+        list.render(area, &mut buf, &mut state);
+
+        // then
+        assert_buffer_eq(buf, Buffer::with_lines(vec!["     ", "     ", "     "]))
     }
 
     #[test]
